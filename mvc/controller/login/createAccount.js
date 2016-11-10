45be9d9f3f4cc9639a1a287ed37cc30f813f9bdb
Erick Sauri <ejs3863@rit.edu>
2016-11-10 07:47:43 -0500

    Added recaptcha. Remade create account with fewer fields since we’re detaching democragphics from users

--- mvc/controller/login/createAccount.js
+++ mvc/controller/login/createAccount.js
@@ -1,31 +1,49 @@
 var validator = require('validator'),
-successNotifications = [], errorNotifications = [];
+    recaptcha = require('express-recaptcha'),
+    successNotifications = [], errorNotifications = [];
 
 module.exports = function(app){
-  app.get('/createaccount', function(req,res){
-    // If user hasn't logged in
-    if (!req.user) {
-      // Redirect to auth/google so they can log in
-      res.redirect('/auth/google');
-    }
+  app.get('/createaccount', recaptcha.middleware.render, function(req,res) {
 
-    // Otherwise render page
+      renderCreateAccount(req, res);
+  });
+
+  app.post('/createaccount', recaptcha.middleware.verify, function(req, res) {
+    var model = {};
+
+    // Reset error notifications
+    errorNotifications.length = 0;
+
+    if (!req.recaptcha.error) {
+      createUserAccount(req, res);
+    }
     else {
+      errorNotifications.push(req.captcha.error);
       renderCreateAccount(req, res);
     }
   });
 
-  app.post('/createaccount', function(req, res) {
-    var model = {},
-        rb = req.body,
+  function renderCreateAccount (req, res) {
+    var model = require('../../model/global')(req, res),
+        user;
+
+    model.content.pageTitle = 'Create Account';
+    model.captcha = req.recaptcha;
+
+    // If we have error notifications
+    // attach them to the model so they can be used in the view
+    model.errorNotifications = (errorNotifications.length > 0) ? errorNotifications : null;
+
+    res.render('login/createAccount', model);
+
+
+  }
+
+  function createUserAccount (req, res) {
+    var rb = req.body,
         userData = {},
-        userRace = '',
-        userRaceLength,
         validUserData,
         encrypt = require('../../model/encrypt');
-
-    // Reset error notifications
-    errorNotifications.length = 0;
 
     // If user submits information
     if (req.body.userInfoSubmit) {
@@ -37,32 +55,28 @@ module.exports = function(app){
         // Make an object for user data
         userData = {
           userName: rb.username,
-          email: req.user.emails[0].value,
+          email: rb.email,
           password: (rb.password)? encrypt(rb.password) : undefined,
-          passportType: rb.passportType,
-          firstName: rb.firstName,
-          lastName: rb.lastName,
+          passportType: 'local',
           displayName: rb.displayName,
-          gender: rb.gender,
-          ethnicity: rb.latino,
-          race: rb.race,
-          major: rb.major,
-          schoolYear: rb.schoolYear,
-          deafCommunity: rb.deafCommunity,
-          languages: rb.languages,
-          mathCoursesTaken: rb.mathCoursesTaken,
-          statisticsCoursesTaken: rb.statsCoursesTaken,
-          readingLevel: rb.readingLevel,
+          deaf: rb.deaf,
           research: 'opt-in',
           role: 5,
         }
-        console.log(userData);
+
         require('../../model/insertUser')(req, userData, function(err, success) {
           if (err) {
             console.error(err);
             res.redirect('/createaccount');
           }
           else {
+            req.user = {
+              userName: userData.userName,
+              password: userData.password
+            };
+
+            res.cookie('username', req.user.userName);
+            res.cookie('password', req.user.password);
             // Allow user to login
              res.redirect('/dashboard');
           }
@@ -81,40 +95,13 @@ module.exports = function(app){
       // Redirect us back to this page
       res.redirect('/createaccount');
     }
-  });
-
-  function renderCreateAccount (req, res) {
-    var model = require('../../model/global')(req, res),
-        user;
-
-    model.content.pageTitle = 'Create Account';
-
-    // Otherwise if user logged in with a google account
-      user = req.user;
-
-      model.user = {
-        email: user.emails[0].value,
-        displayName: user.displayName,
-        firstName: user.name.givenName,
-        lastName: user.name.familyName
-      }
-
-      // If we have error notifications
-      // attach them to the model so they can be used in the view
-      model.errorNotifications = (errorNotifications.length > 0) ? errorNotifications : null;
-
-      res.render('login/createAccount', model);
-
-
   }
 
   function validateUserInput (req) {
     var valid = true,
         pattern = {
           bool: /no|yes/,
-          gender: /female|male|other/,
-          year: /\d|grad/,
-          reading: /none|beginner|intermediate|advanced/
+          password: /./
         },
         rb = req.body;
 
@@ -124,56 +111,34 @@ module.exports = function(app){
       valid = false;
     }
 
+    // If it passes
+    else {
+      // Check if passport is local
+      if (rb.passportType === 'local') {
+        // Verify passwords
+        if (!validator.matches(rb.password, pattern.password)) {
+          errorNotifications.push('Password must beat least 8 chars long with a mixture of uppercase and lowercase letters.');
+          valid = false;
+        }
+
+        if (rb.password !== rb.passwordCheck) {
+          errorNotifications('Passwords do not match!');
+          valid = false;
+        }
+      }
+    }
+
     // email: not null, email
-    if (!validator.isEmail(req.user.emails[0].value)) {
+    if (!validator.isEmail(rb.email)) {
       errorNotifications.push('Please enter a vaid email.');
       valid = false;
     }
 
-    // // school year: 1-5 or grad
-    if (!validator.matches(rb.schoolYear, pattern.year)) {
-      errorNotifications.push('Incorrect school year please try again.');
-      valid = false;
-    }
-
-    // If stats was checked
-    if (rb.statsCoursesTaken) {
-      // Check if value is yes or no
-      if (!validator.matches(rb.statsCoursesTaken, pattern.bool)) {
-        errorNotifications.push('There was an issue selecting your statistic courses please try again.');
-        valid = false;
-      }
-    }
-
-    // If reading level does not match pattern
-    if (!validator.matches(rb.readingLevel, pattern.reading)) {
-      errorNotifications.push('There was an issue selecting your reading level please try again.');
-      valid = false;
-    }
-
-    // If gender was selected
-    if (rb.gender) {
-      // gender: male, female, other
-      if (!validator.matches(rb.gender, pattern.gender)) {
-        errorNotifications.push('There was an issue selecting your gender please try again.');
-        valid = false;
-      }
-    }
-
     // If user chooses deaf community
-    if (rb.deafCommunity) {
+    if (rb.deaf) {
       // deaf: yes, no
-      if (!validator.matches(rb.deafCommunity, pattern.bool)) {
+      if (!validator.matches(rb.deaf, pattern.bool)) {
         errorNotifications.push('There was an issue selecting your affiliation please try again.');
-        valid = false;
-      }
-    }
-
-    // If latino ethnicity is chosen
-    if (rb.latino) {
-      // latino: yes, no
-      if (!validator.matches(rb.latino, pattern.bool)) {
-        errorNotifications.push('There was an issue selecting your ethnicity please try again.');
         valid = false;
       }
     }
