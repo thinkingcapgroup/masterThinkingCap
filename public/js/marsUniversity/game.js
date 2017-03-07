d832601faa66670b3a73afd9c3195491762ba237
Stefen Lewis <sml5527@g.rit.edu>
2017-03-07 14:47:56 -0500

    Cosmetic Fix

--- public/js/marsUniversity/game.js
+++ public/js/marsUniversity/game.js
@@ -971,7 +971,7 @@ function userAction()
     }
 	document.getElementById("Buttons").innerHTML += "<button type='button' onclick='statement()'> Make a Statement - 1 Hour</button>";
 	document.getElementById("Buttons").innerHTML += "<button type='button' class = 'logHelp' onclick='helpScreen()'> Help Screen</button>";
-	document.getElementById("Buttons").innerHTML += "<button type='button' onclick='trendReportMenu()'> View Trend Reports</button>";
+	document.getElementById("Buttons").innerHTML += "<button type='button' onclick='trendReportMenu()'> View Trend Reports</button></br>";
 	//document.getElementById("Buttons").innerHTML += "<button type='button' class='logEventEnd' onclick='gameCycleEnd()'> Skip to the End </button><br>";
 	for(var i=0; i<pastPollResults.length;i++)
 	{
