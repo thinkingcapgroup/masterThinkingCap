4c1ae19d4116cb57214bd27a1374dd244cb707eb
Stefen Lewis <sml5527@g.rit.edu>
2017-04-25 17:05:50 -0400

    Video Area broken css

--- public/js/video.js
+++ public/js/video.js
@@ -5,3 +5,55 @@ function logVideo(x){
 	$.post(where, {videoNum: x});
 	window.location = arrayDirect[x-1]
 }
+
+function tabChange(evt, tabID){
+	var i,
+	tablinks,
+  currentTab = 0,
+  currentsubTab = 0,
+	tabcontent;
+
+	tabcontent = document.getElementsByClassName("tabcontent");
+
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    var elementID = "report"+tabID;
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(elementID).style.display = "block";
+    evt.currentTarget.className += " active";
+
+ currentTab = tabID
+}
+
+function subtabChange(evt, tabID){
+  var i,
+  tablinks,
+  currentTab = 0,
+  tabcontent;
+
+  tabcontent = document.getElementsByClassName("subtabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("subtablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    var elementID = "subreport"+tabID;
+    console.log(elementID)
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(elementID).style.display = "block";
+    evt.currentTarget.className += " active";
+
+ currentTab = tabID
+ console.log(currentTab)
+}
