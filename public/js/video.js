17f94d7ae21edd3b2175c3329f3c1218414fce40
Stefen Lewis <sml5527@g.rit.edu>
2017-04-27 13:28:44 -0400

    YOoutube updates

--- public/js/video.js
+++ public/js/video.js
@@ -3,13 +3,93 @@ var videoWatched = false;
 
 window.onload = function()
 {
-    document.getElementById("vid1").addEventListener('click', markVideoWatched());
-    document.getElementById("vid2").addEventListener('click', markVideoWatched());
-    document.getElementById("vid3").addEventListener('click', markVideoWatched());
-    document.getElementById("vid4").addEventListener('click', markVideoWatched());
-    document.getElementById("vid5").addEventListener('click', markVideoWatched());
+    //document.getElementById("vid1").contentWindow.addEventListener('click', markVideoWatched());
+    //document.getElementById("vid2").contentWindow.addEventListener('click', markVideoWatched());
+    //document.getElementById("vid3").contentWindow.addEventListener('click', markVideoWatched());
+    //document.getElementById("vid4").contentWindow.addEventListener('click', markVideoWatched());
+    //document.getElementById("vid5").contentWindow.addEventListener('click', markVideoWatched());
     videoWatched = false;
+    //onYouTubeIframeAPIReady();
 }
+  var tag = document.createElement('script');
+  tag.id = 'iframe-demo';
+  tag.src = 'https://www.youtube.com/iframe_api';
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  var player;
+  function onYouTubeIframeAPIReady() {
+    player = new YT.Player('vid1', {
+        events: {
+          'onReady': onPlayerReady,
+          'onStateChange': onPlayerStateChange
+        }
+    });
+    onYouTubeIframeAPIReady2();
+  }
+  function onYouTubeIframeAPIReady2() {
+    player = new YT.Player('vid2', {
+        events: {
+          'onReady': onPlayerReady,
+          'onStateChange': onPlayerStateChange
+        }
+    });
+    onYouTubeIframeAPIReady3();
+  }
+  function onYouTubeIframeAPIReady3() {
+    player = new YT.Player('vid3', {
+        events: {
+          'onReady': onPlayerReady,
+          'onStateChange': onPlayerStateChange
+        }
+    });
+    onYouTubeIframeAPIReady4();
+  }
+  function onYouTubeIframeAPIReady4() {
+    player = new YT.Player('vid4', {
+        events: {
+          'onReady': onPlayerReady,
+          'onStateChange': onPlayerStateChange
+        }
+    });
+    onYouTubeIframeAPIReady5();
+  }
+  function onYouTubeIframeAPIReady5() {
+    player = new YT.Player('vid5', {
+        events: {
+          'onReady': onPlayerReady,
+          'onStateChange': onPlayerStateChange
+        }
+    });
+    console.log("bllllll")
+  }
+  function onPlayerReady(event) {
+    console.log("hate");
+  }
+
+  function changeBorderColor(playerStatus) {
+    var color;
+    if (playerStatus == -1) {
+      color = "#37474F"; // unstarted = gray
+    } else if (playerStatus == 0) {
+      color = "#FFFF00"; // ended = yellow
+    } else if (playerStatus == 1) {
+      color = "#33691E"; // playing = green
+    } else if (playerStatus == 2) {
+      color = "#DD2C00"; // paused = red
+    } else if (playerStatus == 3) {
+      color = "#AA00FF"; // buffering = purple
+    } else if (playerStatus == 5) {
+      color = "#FF6DOO"; // video cued = orange
+    }
+    if (color) {
+      document.getElementById('existing-iframe-example').style.borderColor = color;
+    }
+  }
+  function onPlayerStateChange(event) {
+    markVideoWatched();
+  }
+
 function markVideoWatched()
 {
     console.log("shakalaka")
